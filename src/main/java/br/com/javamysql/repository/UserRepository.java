913435d4e0a9eb27572588c63f187b1047bf3e6f
Felipe Amador <143852525+felipetosma@users.noreply.github.com>
2024-10-16 17:02:20 -0300

    update

--- src/main/java/br/com/javamysql/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.javamysql.repository;
-
-import br.com.javamysql.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {}
